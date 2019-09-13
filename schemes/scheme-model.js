e76b1389c02d5b4ee24ecbfd5117e4bfb0c1bd94
Desiree Morris <desiquinncompany@gmail.com>
2019-09-13 06:41:26 -0400

    hit mvp by formatting models and testing endpoints

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -10,25 +10,47 @@ module.exports= {
 }
 
 function find() {
+    return db('schemes');
+}
+
+function findById(id) {
     return db('schemes')
+        .where({id})
+        .first()
 }
 
-function findById() {
-
+function findSteps(id) {
+    return db('steps')
+        .select([
+            'steps.id',
+            'schemes.scheme_name',
+            'steps.step_number',
+            'steps.instructions',
+        ])
+        .join('schemes', 'schemes.id', 'steps.scheme_id')
+        .orderBy('steps.step_number')
+        .where({'scheme_id': id})
 }
 
-function findSteps() {
-
+function add(scheme) {
+    return db('schemes')
+        .insert(scheme)
+        .then(([id]) => findById(id));
 }
 
-function add() {
-
+function update(scheme, id) {
+    return db('schemes')
+        .where({id})
+        .update(scheme)
+        .then((count) => {
+            if(count) {
+             return findById(id)
+            }
+        });
 }
 
-function update() {
-
-}
-
-function remove() {
-    
+function remove(id) {
+    return db('schemes')
+        .where({id})
+        .del()
 }
